20b2187cba172ceb91665f683836f667c6fada25
troy-hamilton <121830956+troy-hamilton@users.noreply.github.com>
2023-02-05 19:59:41 -0600

    Update fptofrac.cpp

--- fptofrac.cpp
+++ fptofrac.cpp
@@ -1,19 +1,17 @@
-#include <iostream>
 #include <valarray> 
-
-using namespace std;
 
 void fptofrac(double fp, int &numerator, int &denominator)
 {
-	double precision = 5e-4;
-	int cycles = 10;
-	int sign = fp > 0 ? 1 : -1;
-	fp = fp * sign;
-	double new_fp, whole_part;
-	double decimal_part = fp - (int)fp;
-	int counter = 0;
-
-	valarray<double> vec_1{double((int) fp), 1}, vec_2{1,0}, temporary;
+	double
+		precision = 5e-4, new_fp, whole_part,
+		decimal_part = fp - (int)fp;
+	
+	int
+		cycles = 10, sign = fp > 0 ? 1 : -1, counter = 0;
+	
+	std::valarray<double> vec_1{double((int) fp), 1}, vec_2{1,0}, temporary;
+	
+	fp *= sign;
 
 	while(decimal_part > precision & counter < cycles)
 	{
@@ -32,17 +30,18 @@ void fptofrac(double fp, int &numerator, int &denominator)
 	denominator = (int)vec_1[1];
 }
 
-using std:: cout;
-using std::endl;
+#include <iostream>
 
 int main()
 {
+	using std::cout;
+	using std::endl;
 	int n, d;
-	double fpnum = 7.0 / 16;
+	double fpnum = 77.0 / 38.0;
 	
 	fptofrac(fpnum, n, d);
 	
-	// outputs 7 / 16
+	// outputs 77 / 38
 	cout << "floating point value: " << fpnum << endl 
 	<< "fractional value: " << n << " / " << d << endl;
 }
